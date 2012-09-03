a639e1fde1f3b24b5f3ce1e0345b9a83c07b7d33
Chen Weigang <samanthena84@hotmail.com>
2012-09-03 21:08:33 +0800

    Under version control

---
+++ VersionControl/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  VersionControl
+//
+//  Created by Chen Weigang on 9/3/12.
+//  Copyright (c) 2012 Star Target. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ VersionControl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VersionControl
+//
+//  Created by Chen Weigang on 9/3/12.
+//  Copyright (c) 2012 Star Target. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
